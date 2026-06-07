b142e6c41e4ece7d8ba60692f4f73803545ea1c2
Soupborsh <100529201+Soupborsh@users.noreply.github.com>
2026-06-07 20:59:27 +0500

    Make license LGPL-3.0-only and add MIT notice to original Sparkfun Arduino C++ library

--- src/lib.rs
+++ src/lib.rs
@@ -11,5 +11,15 @@ pub use otos::SparkfunOTOS;
 
 use crate::driver::otos;
 
+// The official Arduino library: github.com/sparkfun/SparkFun_Qwiic_OTOS_Arduino_Library
+// which is licensed under MIT license by Sparkfun Electronics
+// has been used a reference for writing this driver,
+// some code portions were copied and modified like registers,
+// some were copied as is, like some documentation comments.
+// Thank you Sparkfun Electronics!
+// You can find the notice in third-party-license-notice.md file
+// in root directory of this repository.
+// TODO: License properly?
+
 pub const PRODUCT_ID: u8 = 0x5F;
 pub const DEFAULT_ADDR: u8 = 0x17;
